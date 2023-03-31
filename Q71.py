2bda520394675a2afcf28e7f5cd7c362cea4a9f6
kristadoc23 <123026546+kristadoc23@users.noreply.github.com>
2023-03-31 11:49:46 -0400

    Add files via upload

---
+++ Q71.py
@@ -0,0 +1,8 @@
+#Krista Dockery
+def count_hashtag(string):
+    index = 0
+    while index < len(string):
+        
+        print(number of '#')
+
+count_hashtag("h#o#w m#a#n#y")
---
+++ Q72.py
@@ -0,0 +1,7 @@
+#Krista Dockery
+def hash_spam(string):
+    index = 0
+    if ("#" in string >= 4)
+        return ("this tweet will be considered as a spam!")
+    else:
+        return None
